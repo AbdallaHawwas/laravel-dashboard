623ced965db795726fc0e9de48c1d05878cdbab9
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2023-05-18 22:07:52 +0300

    allow status component to have more than message.

--- resources/components/status.blade.php
+++ resources/components/status.blade.php
@@ -9,7 +9,16 @@
 
 @if (session()->has($status))
     <div {{ $attributes->merge(['class' => 'alert alert-dismissible alert-' . $color]) }}>
-        {{ session($status) }}
+        @if (is_array(session($status)))
+            <ul class="mb-0">
+                @foreach (session($status) as $message)
+                    <li>{{ $message }}</li>
+                @endforeach
+            </ul>
+        @else
+            {{ session($status) }}
+        @endif
+
         <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
     </div>
 @endif
