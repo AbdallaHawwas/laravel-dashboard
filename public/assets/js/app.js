a0829a1aadfe97ff051a157f414fae96b8f345e3
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2023-09-23 15:42:00 +0300

    ask before delete when using livewire datatable

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -20,3 +20,30 @@ jconfirm.defaults = {
 
     // ...
 };
+
+/* ---------------------------------
+ * Redot Livewire Datatables
+ * --------------------------------- */
+
+$(document).on('submit', '.livewire-datatable form', (event) => {
+    event.preventDefault();
+
+    $.confirm({
+        type: 'red',
+        title: __('Are you sure?'),
+        content: __('This action is irreversible.'),
+        buttons: {
+            confirm: {
+                text: __('Yes'),
+                btnClass: 'btn-primary',
+                action: () => {
+                    event.target.submit();
+                },
+            },
+            cancel: {
+                text: __('No'),
+                btnClass: 'btn-default',
+            },
+        },
+    });
+});
